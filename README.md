000e038fb6e5059fc0bc05089008ff8ae5e57044
magmag003 <kenta7350u@gmail.com>
2026-09-03 10:02:32 +0900

    Add create_folder

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # UTgroup
+
+
+## フォルダ作成
+create_folders.bat を実行することで、必要なフォルダが作成されます。
---
+++ create_folders.bat
@@ -0,0 +1,15 @@
+@echo off
+cd /d "%~dp0"
+
+mkdir "COMSOL\source"
+mkdir "COMSOL\results"
+mkdir "ComWAVE\source"
+mkdir "ComWAVE\results"
+mkdir "Experiment\source"
+mkdir "Experiment\source\MasterData"
+mkdir "Experiment\results"
+mkdir "JMAG\source"
+mkdir "JMAG\results"
+
+echo フォルダ作成が完了しました。
+pause
